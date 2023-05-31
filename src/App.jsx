158f490c9e6d0b71c87c82868980d70cdbe5055f
Victor Emanoel <victoremaanoel@outlook.com>
2023-05-31 01:00:33 -0300

    created folder structure and navBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import logo from './assets/movie.png'
-import './App.css'
+import NavBar from './components/navBar/NavBar'
 
 function App() {
   return (
-    <>
-      <div>
-        <a>
-          <img src={logo} className="logo"/>
-        </a>
-        <h2>My Movies</h2>
-      </div>
-      <div className="card">
-        <button onClick={() => null}>
-          Pesquisar
-        </button>
-      </div>
-      <footer>My Movies &copy; 2023</footer>
-    </>
+    <div className="App">
+      <NavBar/>
+    </div>
   )
 }
 
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import logo from '../../assets/movie.png'
+import { Link } from 'react-router-dom';
+import {BiSearchAlt2, BiStar, BiInfoCircle} from 'react-icons/bi'
+import { IconContext } from 'react-icons';
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav id="navbar">
+            <h3>
+                <Link to='/'>
+                    <img src={logo} className="logo"/>
+                    My Movies
+                </Link>
+            </h3>
+            <form>
+                <input type="text" placeholder='Pesquise um filme aqui!'/>
+                <button type='submit'>
+                    <BiSearchAlt2/>
+                </button>
+            </form>
+            <div className='nav-links'>
+                <IconContext.Provider value={{ size: '24px', color:'rgb(255, 255, 255, 0.87)'}}>
+                    <Link to='/favorits'> 
+                        <BiStar/>
+                    </Link>
+                    <Link to='/sobre' style={{ marginLeft: '18px'}}><BiInfoCircle/></Link>
+                </IconContext.Provider>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
