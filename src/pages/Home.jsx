5e630b4f4636f3d380f0f4a698ccee8560d753cf
Victor Emanoel <victoremaanoel@outlook.com>
2023-05-31 23:59:47 -0300

    Create 2 new get methods to consume API - Home comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,8 @@ const moviesURL = import.meta.env.VITE_API
 
 const Home = () => {
     const [topRatedMovies, setTopRated] = useState([])
+    const [upcomingMovies, setUpcomingMovies] = useState([])
+    const [popularMovies, setPopularMovies] = useState([])
 
     const getTopRaded = async (url) => {
         const res = await fetch(url)
@@ -14,10 +16,30 @@ const Home = () => {
         console.log(data)
 
     }
+    const getUpcomingMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+        setUpcomingMovies(data.results)
+        console.log(data)
+
+    }
+    const getPopularMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+        setPopularMovies(data.results)
+        console.log(data)
+
+    }
 
     useEffect(() => {
         const topRatedURL = `${moviesURL}top_rated?${apiKey}`
+        const upcomingMoviesURL = `${moviesURL}upcoming?${apiKey}`
+        const popularMoviesURL = `${moviesURL}popular?${apiKey}`
+
         getTopRaded(topRatedURL)
+        getUpcomingMovies(upcomingMoviesURL)
+        getPopularMovies(popularMoviesURL)
+
     },[])
 
     return (
@@ -27,6 +49,24 @@ const Home = () => {
                 {topRatedMovies.length === 0 && <p>Carregando...</p>}
                 {
                     topRatedMovies.map((movie) => 
+                        <MovieCard movie={movie} key={movie.id}/>
+                    )
+                }
+            </div>
+            <h2 className="title">Em Brave</h2>
+            <div className="movies-container">
+                {upcomingMovies.length === 0 && <p>Carregando...</p>}
+                {
+                    upcomingMovies.map((movie) => 
+                        <MovieCard movie={movie} key={movie.id}/>
+                    )
+                }
+            </div>
+            <h2 className="title">Populares</h2>
+            <div className="movies-container">
+                {popularMovies.length === 0 && <p>Carregando...</p>}
+                {
+                    popularMovies.map((movie) => 
                         <MovieCard movie={movie} key={movie.id}/>
                     )
                 }
