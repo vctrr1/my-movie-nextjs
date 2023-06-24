c281353765d06c7786b6af0c10f03ea0f59df0ae
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-24 02:41:02 -0300

    Add function Delete movie to button Desfavoritar

--- src/pages/favorits/Favorits.jsx
+++ src/pages/favorits/Favorits.jsx
@@ -5,6 +5,25 @@ import MovieCard from "../../components/movieCard/MovieCard";
 const Favorits = () => {
   const data = Object.values(movies);
 
+  const deleteMovieFromFavorits = (key) => {
+    fetch(`http://localhost:4100/db/delete-movie/${key}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Objeto JSON deletado com sucesso.");
+        } else {
+          console.error("Erro ao deletar o objeto JSON:", response.status);
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao fazer a requisição:", error);
+      });
+  };
+
   return (
     <div className="container">
       <h2 className="titleFav">Favoritos</h2>
@@ -13,7 +32,10 @@ const Favorits = () => {
           <div className="movie-card" key={index}>
             <MovieCard movie={item} key={item.id} />
             <p>
-              <button className="buttonUFavorit" onClick={null}>
+              <button
+                className="buttonUFavorit"
+                onClick={() => deleteMovieFromFavorits(item.id)}
+              >
                 Desfavoritar
               </button>
             </p>
