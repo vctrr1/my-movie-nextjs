5edeb255966a1f2f40b564762700c2d4502f5383
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-24 01:32:02 -0300

    Implementing Favorits Page

--- src/pages/favorits/Favorits.jsx
+++ src/pages/favorits/Favorits.jsx
@@ -6,10 +6,20 @@ const Favorits = () => {
   const data = Object.values(movies);
 
   return (
-    <div>
-      {data.map((item) => (
-        <MovieCard movie={item} key={item.id} />
-      ))}
+    <div className="container">
+      <h2 className="titleFav">Favoritos</h2>
+      <div className="favorits-movies">
+        {data.map((item, index) => (
+          <div className="movie-card" key={index}>
+            <MovieCard movie={item} key={item.id} />
+            <p>
+              <button className="buttonUFavorit" onClick={null}>
+                Desfavoritar
+              </button>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
