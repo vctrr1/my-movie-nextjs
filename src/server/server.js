e8f2faa27f5d0e038de3ab851bdec5ba32b5409a
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-24 02:07:32 -0300

    feat: New api method to delete favorite movie

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,42 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/db/save-json", (req, res) => {
+app.delete("/db/delete-movie/:key", (req, res) => {
+  const key = req.params.key;
+  const filePath = "data.json";
+
+  fs.readFileSync(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.error("Erro ao ler o arquivo:", err);
+      res.status(500).send("Erro ao excluir o objeto JSON");
+      return;
+    }
+
+    let jsonArray = [];
+    try {
+      jsonArray.JSON.parse(data);
+    } catch (err) {
+      console.error("Erro ao fazer o parse do arquivo JSON:", err);
+      res.status(500).send("Erro ao excluir o objeto JSON");
+      return;
+    }
+
+    const newArray = jsonArray.filter((obj) => obj.id != key);
+    const updatedArrayMovies = JSON.stringify(newArray, null, 2);
+
+    fs.writeFileSync(filePath, updatedArrayMovies, (err) => {
+      if (err) {
+        console.error("Erro ao excluir o objeto JSON:", err);
+        res.status(500).send("Erro ao excluir o objeto JSON");
+      } else {
+        console.log("Objeto JSON excluído com sucesso.");
+        res.status(200).send("Objeto JSON excluído com sucesso.");
+      }
+    });
+  });
+});
+
+app.post("/db/save-movie", (req, res) => {
   const objectData = req.body;
   const filePath = "data.json";
 
