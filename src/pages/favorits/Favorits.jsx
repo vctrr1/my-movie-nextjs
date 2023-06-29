596d4e864967db3b282ba5eff42c3eeb3f9974cf
Victor Emanoel <victoremaanoel@outlook.com>
2023-06-28 22:08:54 -0300

    feat: new method to calculate watched time

--- src/pages/favorits/Favorits.jsx
+++ src/pages/favorits/Favorits.jsx
@@ -1,9 +1,23 @@
 import "./Favorits.css";
+import { useState, useEffect } from "react";
 import movies from "../../server/data.json";
 import MovieCard from "../../components/movieCard/MovieCard";
 
 const Favorits = () => {
+  const [totalRuntime, setTotalRuntime] = useState(0);
   const data = Object.values(movies);
+
+  useEffect(() => {
+    const calculateTotalRuntime = () => {
+      let sum = 0;
+      data.forEach((item) => {
+        sum += item.runtime;
+      });
+      setTotalRuntime(sum);
+    };
+
+    calculateTotalRuntime();
+  }, [data]);
 
   const deleteMovieFromFavorits = (key) => {
     fetch(`http://localhost:4100/db/delete-movie/${key}`, {
@@ -27,6 +41,7 @@ const Favorits = () => {
   return (
     <div className="container">
       <h2 className="titleFav">Favoritos</h2>
+      <div className="I">Tempo total assistido: {totalRuntime} minutes</div>
       <div className="favorits-movies">
         {data.map((item, index) => (
           <div className="movie-card" key={index}>
